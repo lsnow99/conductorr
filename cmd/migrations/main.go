e9acfe8ecc0399d993e77104a4ff4712eef27450
Logan <logansnow@outlook.com>
2019-12-08 05:38:45 -0500

    load database values thru env vars

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -5,19 +5,26 @@ import (
 	"os"
 
 	"github.com/go-pg/pg/v9"
+	"github.com/joho/godotenv"
 	migrations "github.com/robinjoseph08/go-pg-migrations/v2"
 )
 
 const directory = "cmd/migrations"
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Could not load .env file: %s", err.Error())
+	}
+
 	db := pg.Connect(&pg.Options{
-		Addr:     "localhost:5432",
-		User:     "conductorr",
-		Database: "conductorrdb",
+		Addr:     os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Database: os.Getenv(("DB_DATABASE")),
 	})
 
-	err := migrations.Run(db, directory, os.Args)
+	err = migrations.Run(db, directory, os.Args)
 	if err != nil {
 		log.Fatalln(err)
 	}
