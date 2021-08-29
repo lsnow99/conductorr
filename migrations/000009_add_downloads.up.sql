c4ca768686842f02a3bca93dc473b12029739c5f
Logan Snow <logansnow@protonmail.com>
2021-08-29 12:30:03 -0400

    Fix migrations

--- migrations/000009_add_downloads.up.sql
+++ migrations/000009_add_downloads.up.sql
@@ -6,8 +6,8 @@ CREATE TABLE download (
     --postgresql--
     id SERIAL PRIMARY KEY,
     --end
-    media_id INTEGER REFERENCES media(id) ON DELETE SET NULL,
-    downloader_id INTEGER REFERENCES downloader(id) ON DELETE SET NULL,
+    media_id INTEGER REFERENCES media(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED,
+    downloader_id INTEGER REFERENCES downloader(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED,
     status VARCHAR(64) NOT NULL,
     friendly_name TEXT NOT NULL,
     identifier TEXT NOT NULL
