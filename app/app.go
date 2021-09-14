2f1f5dfc1b5f65e3d7d4fbd3a40319dc02022e0d
Logan Snow <logans@convertsation.org>
2021-09-14 08:22:33 -0400

    Library path management

--- app/app.go
+++ app/app.go
@@ -8,11 +8,14 @@ import (
 var DM DownloaderManager
 var IM IndexerManager
 var MSM MediaServerManager
+var LM LibraryManager
 var Monitor SystemMonitor
 
 func init() {
+	LM = NewLibraryManager()
 	Monitor.statusMap = make(map[string]SystemTypeStatus)
 	DM.backupReleaseMap = make(map[int][]integration.Release)
+	scheduler.RegisterTask(&LibraryManager{})
 	scheduler.RegisterTask(&Monitor)
 	scheduler.RegisterTask(&DM)
 	scheduler.RegisterTask(&IM)
