56eb240d110bfb3525d3665b4ad5163f3133d860
Logan Snow <4197432+lsnow99@users.noreply.github.com>
2021-12-02 20:00:11 -0500

    Fix host header forwarding

--- web/vite.config.js
+++ web/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8282',
-        changeOrigin: true,
+        changeOrigin: false,
         secure: false,      
         ws: true,
       }
