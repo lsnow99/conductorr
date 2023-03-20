7eca048b15e5870bf72e132a99f0764d477e0e2b
Logan Snow <4197432+lsnow99@users.noreply.github.com>
2023-03-20 01:23:40 -0400

    experimental qbt support

--- internal/conductorr/integration/qbittorrent.go
+++ internal/conductorr/integration/qbittorrent.go
@@ -1,25 +1,79 @@
 package integration
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+  "net/http/cookiejar"
 
-	"github.com/l3uddz/go-qbittorrent/qbt"
+	"github.com/jackpal/bencode-go"
+	"github.com/lsnow99/conductorr/pkg/constant"
 )
 
 type QBittorrent struct {
-	client *qbt.Client
+	client  *http.Client
+	baseUrl *url.URL
+}
+
+// Non exhaustive struct
+type QBittorrentTorrent struct {
+	AmountLeft  int    `json:"amount_left"`
+	TotalSize   int    `json:"total_size"`
+	State       string `json:"state"`
+	Hash        string `json:"hash"`
+	Name        string `json:"name"`
+	ContentPath string `json:"content_path"`
 }
 
 func NewQBittorrent(username, password, baseUrl string) (*QBittorrent, error) {
 	q := new(QBittorrent)
-	q.client = qbt.NewClient(baseUrl)
-	err := q.client.Login(qbt.LoginOptions{
-		Username: username,
-		Password: password,
-	})
+	var err error
+	q.baseUrl, err = url.Parse(baseUrl)
+	if err != nil {
+		return nil, fmt.Errorf("bad url %s", baseUrl)
+	}
+	q.client = &http.Client{Timeout: time.Duration(1) * time.Second}
+
+	vals := url.Values{
+		"username": []string{username},
+		"password": []string{password},
+	}
+
+	loginUrl := q.baseUrl
+	loginUrl.Path = "api/v2/auth/login"
+
+	resp, err := q.client.PostForm(loginUrl.String(), vals)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("bad status code %d %s", resp.StatusCode, loginUrl.String())
+	}
+
+	cookies := resp.Cookies()
+
+	if len(cookies) == 0 {
+		return nil, fmt.Errorf("received no cookies")
+	}
+
+  q.client.Jar, err = cookiejar.New(nil)
+  if err != nil {
+    return nil, err
+  }
+	q.client.Jar.SetCookies(loginUrl, cookies)
 
 	return q, err
 }
@@ -41,6 +95,8 @@ type TorrentMetadata struct {
 }
 
 func (q *QBittorrent) AddRelease(release Release) (string, error) {
+	addUrl := q.baseUrl
+	addUrl.Path = "api/v2/torrents/add"
 
 	resp, err := http.Get(release.DownloadURL)
 	if err != nil {
@@ -49,19 +105,38 @@ func (q *QBittorrent) AddRelease(release Release) (string, error) {
 
 	defer resp.Body.Close()
 
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
 	tm := TorrentMetadata{}
 
-	// data, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	return "", err
-	// }
+	err = bencode.Unmarshal(bytes.NewBuffer(data), &tm)
+	if err != nil {
+		return "", err
+	}
 
-	// res, err := bencode.Unmarshal(data)
-	// if err != nil {
-	// 	return "", err
-	// }
+	buf := bytes.NewBuffer(data)
+	writer := multipart.NewWriter(buf)
 
-	// fmt.Println(res)
+	req, err := http.NewRequest(http.MethodPost, addUrl.String(), buf)
+
+	if err != nil {
+		return "", nil
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err = q.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("non 200 status code %d", resp.StatusCode)
+	}
 
 	h := sha1.Sum(tm.Info)
 	hashStr := hex.EncodeToString(h[:])
@@ -69,19 +144,111 @@ func (q *QBittorrent) AddRelease(release Release) (string, error) {
 }
 
 func (q *QBittorrent) DeleteDownload(identifier string) error {
-	// Delete the torrent and associated files
-	ok, err := q.client.Delete([]string{identifier}, true)
-	if !ok {
+	deleteUrl := q.baseUrl
+	deleteUrl.Path = "api/v2/torrents/delete"
+
+	vals := url.Values{
+		"hashes":      []string{identifier},
+		"deleteFiles": []string{strconv.FormatBool(false)},
+	}
+
+	resp, err := q.client.PostForm(deleteUrl.String(), vals)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != 200 {
 		return fmt.Errorf("qbittorrent could not delete download with id %s", identifier)
 	}
-	return err
+	return nil
 }
 
 func (q *QBittorrent) PollDownloads(identifiers []string) ([]Download, error) {
-	return nil, nil
+	listUrl := q.baseUrl
+	listUrl.Path = "api/v2/torrents/info"
+
+	vals := url.Values{
+		"hashes": []string{strings.Join(identifiers, "|")},
+	}
+
+	listUrl.RawQuery = vals.Encode()
+
+	resp, err := q.client.Get(listUrl.String())
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	torrents := make([]QBittorrentTorrent, 0, len(identifiers))
+
+	if err := json.NewDecoder(resp.Body).Decode(&torrents); err != nil {
+		return nil, err
+	}
+
+	downloads := make([]Download, 0, len(torrents))
+
+	for _, torrent := range torrents {
+		d := Download{}
+
+		if torrent.State == "error" || torrent.State == "missingFiles" || torrent.State == "unknown" {
+			d.Status = constant.StatusError
+		}
+
+		if torrent.State == "stalledDL" || torrent.State == "queuedDL" || torrent.State == "allocating" || torrent.State == "metaDL" || torrent.State == "checkingResumeData" {
+			d.Status = constant.StatusWaiting
+		}
+
+		if torrent.State == "moving" || torrent.State == "checkingUp" {
+			d.Status = constant.StatusProcessing
+		}
+
+		if torrent.State == "forcedUp" || torrent.State == "stalledUp" || torrent.State == "pausedUp" || torrent.State == "queuedUp" || torrent.State == "uploading" {
+			d.Status = constant.StatusComplete
+		}
+
+		if torrent.State == "pausedDL" {
+			d.Status = constant.StatusPaused
+		}
+
+		if torrent.State == "downloading" {
+			d.Status = constant.StatusDownloading
+		}
+
+		if d.Status == constant.StatusComplete {
+			d.FinalDir = torrent.ContentPath
+		}
+
+		d.BytesLeft = uint64(torrent.AmountLeft)
+		d.FullSize = uint64(torrent.TotalSize)
+		d.FriendlyName = torrent.Name
+		d.Identifier = torrent.Hash
+		downloads = append(downloads, d)
+	}
+
+	return downloads, nil
 }
 
 func (q *QBittorrent) TestConnection() error {
-	_, err := q.client.Info()
-	return err
+	infoUrl := q.baseUrl
+	infoUrl.Path = "api/v2/transfer/info"
+
+	resp, err := q.client.Get(infoUrl.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return err
+	}
+
+	if !json.Valid(body) {
+		return fmt.Errorf("error parsing info response")
+	}
+
+	return nil
 }
